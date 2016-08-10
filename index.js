d5f7780d6c87d3c558a42eb7ec8721756f31fc7a
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-08-10 11:42:57 -0500

    initial tracking

---
+++ index.js
@@ -0,0 +1,110 @@
+var irc = require('slate-irc');
+var net = require('net');
+var KeenTracking = require('keen-tracking');
+
+var logger = require('debug')('app:logger');
+var notice = require('debug')('app:notice');
+
+require('dotenv').config();
+
+var track = new KeenTracking({
+  projectId: process.env.KEEN_ID,
+  writeKey: process.env.KEEN_KEY
+});
+
+var stream = net.connect({
+  port: 6667,
+  host: 'irc.freenode.org'
+});
+
+var client = irc(stream);
+
+client.use(function() {
+  return function(irc){
+    irc.stream.pipe(logger);
+  }
+});
+
+client.nick(process.env.BOT_NICKNAME);
+client.user(process.env.BOT_NICKNAME, process.env.BOT_REALNAME);
+
+var CHANNELS = [
+  '#express',
+  '#node.js',
+  '#react',
+  '#redux',
+  '#openshift',
+  '#css',
+  '#html'
+];
+
+var USERS = [
+  'avilano',
+  'erecinos',
+  'jllanas',
+  'javmarr',
+  'joelgarza',
+  'pamsny',
+  'riccochapa',
+  'samcio',
+  'stevealvaradorgv'
+];
+
+// not in production
+if (!process.env.OPENSHIFT_NODEJS_IP) {
+  USERS.push('ibolmo');
+}
+
+var isTracked = function(user){
+  return USERS.indexOf(user) > -1;
+};
+
+client.join(CHANNELS);
+
+CHANNELS.forEach(function(channel){
+  client.names(channel, function(err, names){
+    if (err) {
+      logger('err "' + err + '" in names for ' + channel);
+    } else {
+      names.forEach(function(user){
+        if (isTracked(user.name)) {
+          notice(channel + ' (found): ' + user.name);
+          track.recordEvent('found', {
+            user: user.name,
+            channel: channel
+          });
+        }
+      });
+    }
+  });
+})
+
+client.on('message', function(e){
+  if (isTracked(e.nick) && e.to != process.env.BOT_NICKNAME) {
+    notice(e.to + ' (message): ' + e.nick);
+    track.recordEvent('message', {
+      user: e.nick,
+      channel: e.to
+    });
+  }
+});
+
+client.on('join', function(e){
+  if (isTracked(e.nick)) {
+    notice(e.channel + ' (join): ' + e.nick);
+    track.recordEvent('join', {
+      user: e.nick,
+      channel: e.channel
+    });
+  }
+});
+
+client.on('part', function(e){
+  if (isTracked(e.nick)) {
+    notice(e.channel + ' (part): ' + e.nick);
+    track.recordEvent('part', {
+      user: e.nick,
+      channel: e.channel
+    });
+  }
+});
